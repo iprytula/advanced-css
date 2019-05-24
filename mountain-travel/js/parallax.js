ee23a56c379e9222fdb25d940f80c7b7eefe5fa7
Iurii Prytula <iprytula@insert-ad.insert.intranet>
2019-05-24 11:43:55 +0200

    newsletter & footer

--- mountain-travel/js/parallax.js
+++ mountain-travel/js/parallax.js
@@ -2,7 +2,7 @@
   $(document).ready(function () {
     $(window).scroll(function () {
       $('.hero').css({
-        'background-position-y': (window.scrollY / 5) + 'px'
+        'background-position-y': (window.scrollY / 2.5) + 'px'
       });
     });
   });
